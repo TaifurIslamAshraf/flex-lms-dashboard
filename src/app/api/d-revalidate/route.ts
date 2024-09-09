b6ea747000339a48b12347f8649ed156881aaaf7
MD Taifur <Taifurislamashraf@gmail.com>
2024-09-10 05:44:29 +0600

    [fix] validate route

--- src/app/api/d-revalidate/route.ts
+++ src/app/api/d-revalidate/route.ts
@@ -1,12 +1,12 @@
 import { customRevalidateTag } from "@/lib/_actions/revalidateTag";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   const { tag } = await req.json();
 
   await customRevalidateTag(tag);
 
-  return Response.json({
+  return NextResponse.json({
     message: `${tag} Validate successfully`,
   });
 }
